2530f92a33ac8b388556f30596ecef63977a9572
Giannis Smirnios <smirnios.giannis@gmail.com>
2018-09-15 16:40:31 +0300

    Routing initial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AirportsListComponent } from './airports/airports-list/airports-list.component';
 import { MapComponent } from './map/map.component';
+import { PortsListComponent } from './ports/ports-list/ports-list.component';
 
 export const routes: Routes = [
   { path: 'airports', component: AirportsListComponent },
+  { path: 'ports', component: PortsListComponent },
   { path: 'map', component: MapComponent }
 ];
 

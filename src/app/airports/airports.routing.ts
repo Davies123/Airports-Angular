ca25cac4397b768eeccee6eb64b27dbf2c666808
sisko <siskogeorge@gmail.com>
2018-09-16 00:23:36 +0300

    Including tips for best practice for routing & navigation.

--- src/app/airports/airports.routing.ts
+++ src/app/airports/airports.routing.ts
@@ -1,9 +1,13 @@
 import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import { AirportsListComponent } from './airports-list/airports-list.component';
+import { AirportsDetailsComponent } from './airports-details/airports-details.component';
 
 const routes: Routes = [
-    { path: 'list', component: AirportsListComponent },
+    { path: '', component: AirportsDetailsComponent },
+    { path: 'list', component: AirportsListComponent }
 ];
 
 export const AirportsRouting: ModuleWithProviders = RouterModule.forChild(routes);
+// Array for routing components
+export const airportRoutingComponents = [AirportsListComponent, AirportsDetailsComponent]     // Tip
--- src/app/ports/ports.routing.ts
+++ src/app/ports/ports.routing.ts
@@ -1,9 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import { PortsListComponent } from './ports-list/ports-list.component';
+import { PortsDetailsComponent } from './ports-details/ports-details.component';
 
 const routes: Routes = [
+    { path: '', component: PortsDetailsComponent },
     { path: 'list', component: PortsListComponent },
 ];
 
 export const PortsRouting: ModuleWithProviders = RouterModule.forChild(routes);
+export const portRoutingComponents = [ PortsListComponent, PortsDetailsComponent];      // Tip
